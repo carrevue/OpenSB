2a8e85cc166c6ec8593634104f8ed8e30930c73e
Chaziz <chazpelo2@gmail.com>
2025-04-12 06:18:08 -0400

    bump to beta 7

--- private/class/SquareBracket/VersionNumber.php
+++ private/class/SquareBracket/VersionNumber.php
@@ -8,7 +8,7 @@ class VersionNumber
     private string $versionString;
 
     public function __construct() {
-        $this->versionNumber = "1.3.0-beta.6";
+        $this->versionNumber = "1.3.0-beta.7";
         $this->versionString = $this->makeVersionString();
     }
 
