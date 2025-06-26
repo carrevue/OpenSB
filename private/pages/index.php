8392968f9f1284730bd778ebd1e5ba623e67ae14
Chaziz <chazpelo2@gmail.com>
2025-06-26 02:03:14 -0400

    add new experimental homepage design
    its supposed to be a feed... well... its a very early implementation of one.

--- private/pages/index.php
+++ private/pages/index.php
@@ -12,6 +12,8 @@ $submission_query = new UploadQuery($database);
 
 $options = $orange->getLocalOptions();
 
+$enable_new_trinium_feed = isset($options["trinium_new_shit"]) && $options["trinium_new_shit"] == "true";
+
 if ($options["skin"] == "trinium") {
     $type = isset($options["trinium_homepage_type"]) && $options["trinium_homepage_type"] !== "list" ? $options["trinium_homepage_type"] : "list";
 } else {
@@ -33,29 +35,39 @@ if ($options["skin"] == "biscuit" || $options["skin"] == "trinium") {
 $submissions_featured_query_limit = 4;
 
 if ($options["skin"] == "bootstrap") {
-    // bootstrap frontend did not list random uploads.
-    $submissions_random = [];
     $news_recent_query_limit = 1;
 } else {
-    $submissions_random = $submission_query->query("RAND()", $submissions_random_query_limit);
     $news_recent_query_limit = 5;
+}
+
+if ($options["skin"] == "bootstrap" || ($options["skin"] == "trinium" && $enable_new_trinium_feed && $type != "grid")) {
+    $submissions_random = [];
+} else {
+    $submissions_random = $submission_query->query("RAND()", $submissions_random_query_limit);
 }
 
 $submissions_recent = $submission_query->query("v.time DESC", $submissions_recent_query_limit);
 
 $featured_flag_bullshit = UploadFlags::FLAG_FEATURED->value; // looks like shit -chaziz 1/3/2025
-$submissions_featured = $submission_query->query("v.time DESC", $submissions_featured_query_limit,
-    "v.flags & $featured_flag_bullshit = $featured_flag_bullshit");
+if ($options["skin"] != "trinium" && !$enable_new_trinium_feed) {
+    $submissions_featured = $submission_query->query("v.time DESC", $submissions_featured_query_limit,
+        "v.flags & $featured_flag_bullshit = $featured_flag_bullshit");
+} else {
+    $submissions_featured = [];
+}
 
-$news_recent = $database->fetchArray($database->query("SELECT j.* FROM journals j WHERE j.is_site_news = 1 ORDER BY j.date DESC LIMIT $news_recent_query_limit"));
-//$users_recent = $database->fetchArray($database->query("SELECT u.id, u.about, u.title, (SELECT COUNT(*) FROM uploads WHERE author = u.id) AS s_num, (SELECT COUNT(*) FROM journals WHERE author = u.id) AS j_num FROM users u ORDER BY u.lastview DESC LIMIT 8"));
+
+if ($options["skin"] == "trinium" && $enable_new_trinium_feed && $type != "grid") {
+    $news_recent = $database->fetchArray($database->query("SELECT j.* FROM journals j ORDER BY j.date DESC LIMIT $news_recent_query_limit"));
+} else {
+    $news_recent = $database->fetchArray($database->query("SELECT j.* FROM journals j WHERE j.is_site_news = 1 ORDER BY j.date DESC LIMIT $submissions_recent_query_limit"));
+}
 
 $data = [
     "submissions" => Utilities::makeUploadArray($database, $submissions_random),
     "submissions_new" => Utilities::makeUploadArray($database, $submissions_recent),
     "submissions_featured" => Utilities::makeUploadArray($database, $submissions_featured),
     "news_recent" => Utilities::makeJournalArray($database, $news_recent),
-    //"users_recent" => $users_recent, // TODO: makeUsersArray
 ];
 
 echo $twig->render('index.twig', [
