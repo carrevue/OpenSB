a78c7621d707c8286737db29c6abfc21eb122526
Chaziz <chazpelo2@gmail.com>
2025-07-17 08:49:25 -0400

    fix copyright on versionnumber.php

--- private/class/SquareBracket/VersionNumber.php
+++ private/class/SquareBracket/VersionNumber.php
@@ -3,9 +3,7 @@
 /*
   OpenSB: The Open SquareBracket Software
 
-  Copyright (C) 2021-2025 Chaziz
-  Copyright (C) 2021 ROllerozxa
-  Copyright (C) 2021-2022 icanttellyou
+  Copyright (C) 2023-2025 Chaziz
 
   OpenSB is free software: you can redistribute it and/or modify it under the 
   terms of the GNU Affero General Public License as published by the Free 
