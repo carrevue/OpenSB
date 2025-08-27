0678093dc94555c6514e71948e44277a2ee237fc
Chaziz <chazpelo2@gmail.com>
2025-08-27 13:01:37 -0400

    fix typo causing nothing to process at all

--- private/scripts/processingworker.php
+++ private/scripts/processingworker.php
@@ -109,7 +109,7 @@ $for_website = $argv[4];
 
 log("Upload type: " .  $upload_type);
 
-if ($upload_type != "video" | $upload_type != "video_thumbnail_only") {
+if ($upload_type != "video" || $upload_type != "video_thumbnail_only") {
     log("Unsupported type.");
     die();
 }
