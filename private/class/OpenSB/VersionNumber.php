7dc13382c6d350f920a10107aca4e681ee094915
Chaziz <chazpelo2@gmail.com>
2025-09-18 21:40:05 -0400

    bump to rc1 c

--- private/class/OpenSB/VersionNumber.php
+++ private/class/OpenSB/VersionNumber.php
@@ -33,7 +33,7 @@ class VersionNumber
     public function __construct()
     {
         $this->versionName = "Brightney";
-        $this->versionNumber = "2.0.0-rc.1.b";
+        $this->versionNumber = "2.0.0-rc.1.c;
         $this->versionString = $this->makeVersionString();
     }
 
