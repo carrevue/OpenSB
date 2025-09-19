df6f42f61b4b3fa6c626f6f2d26bdc04b7a2583a
Chaziz <chazpelo2@gmail.com>
2025-09-19 01:50:51 -0400

    fix db profiler not showing auth-class related queries

--- private/class/OpenSB/SquareBracket.php
+++ private/class/OpenSB/SquareBracket.php
@@ -75,6 +75,12 @@ class SquareBracket
         $this->is_debug = ($config["mode"] ?? '') === "DEV";
 
         $this->database = new Database($host, $user, $pass, $db);
+        $this->profiler = new Profiler($this->database);
+        if ($this->is_debug) {
+            // enable db profiler (not to be confused with the other profiler)
+            // if we are on debug mode
+            $this->database->setProfiling(true);
+        }
         $this->authentication = new Authentication($this->database);
 
         // super dangerous if misused, but that site mode is only intended for
@@ -87,15 +93,11 @@ class SquareBracket
             $this->authentication->getUserData()["id"] == 1
         ) {
             $this->is_debug = true;
+            // this doesnt show auth-related queries however, but it doesnt really matter.
+            $this->database->setProfiling(true);
         }
 
         //$this->version_number = new VersionNumber();
-        $this->profiler = new Profiler($this->database);
-        if ($this->is_debug) {
-            // enable db profiler (not to be confused with the other profiler)
-            // if we are on debug mode
-            $this->database->setProfiling(true);
-        }
 
         $this->storage = new Storage($this);
 
