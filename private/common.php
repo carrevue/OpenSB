ca2a120b5244aa568fdd054e386f72405b15768e
Chaziz <chazpelo2@gmail.com>
2025-09-20 03:12:10 -0400

    fix label on github link

--- private/common.php
+++ private/common.php
@@ -151,7 +151,7 @@ set_exception_handler(function ($exception) {
         );
 
         $githubNewIssueUrl = sprintf(
-            'https://github.com/bluffingo/opensb/issues/new?title=%s&labels=error&body=%s',
+            'https://github.com/bluffingo/opensb/issues/new?title=%s&labels=bug&body=%s',
             urlencode('Error: ' . $exception->getMessage()),
             urlencode(
                 "**Error**: " . $exception->getMessage() . "\n\n" .
