7b3b801dc2258d1063e38bb7f30da343db0ed488
Chaziz <chazpelo2@gmail.com>
2025-12-06 22:06:50 -0500

    bump to 2.0.10

--- private/class/OpenSB/VersionNumber.php
+++ private/class/OpenSB/VersionNumber.php
@@ -41,7 +41,7 @@ class VersionNumber
     /**
      * @var string The version number, which tries to follow Semantic versioning.
      */
-    private string $versionNumber = "2.0.9";
+    private string $versionNumber = "2.0.10";
 
     /**
      * @var string The full complete version string.
