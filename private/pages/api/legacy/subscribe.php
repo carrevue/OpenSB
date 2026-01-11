a4662444798f4fef8d9a7d9be0a547d9254a94c5
Chaziz <chazpelo2@gmail.com>
2026-01-11 01:42:54 -0500

    fix legacy subscribe api on fulphiker

--- private/pages/api/legacy/subscribe.php
+++ private/pages/api/legacy/subscribe.php
@@ -3,7 +3,7 @@
 /*
   OpenSB: The Open SquareBracket Software
 
-  Copyright (C) 2021-2025 Chaziz
+  Copyright (C) 2021-2026 Chaziz
   Copyright (C) 2021-2022 icanttellyou
 
   OpenSB is free software: you can redistribute it and/or modify it under the 
@@ -22,15 +22,17 @@
 
 namespace OpenSB\Pages\LegacyFrontendAPI;
 
-global $auth, $database;
+global $auth, $database, $sb;
+
+$localization = $sb->getLocalizationClass();
 
 if (!isset($_POST['subscription']) or $_POST['subscription'] == '') {
     die(); //don't output anything if this sneaky bastard didn't put anything to the comment field
 }
 if ($database->result("SELECT COUNT(user) FROM user_follows WHERE user=? AND id=?", [$auth->getUserID(), $_POST['subscription']]) != 0) {
     $database->query("DELETE FROM user_follows WHERE user=? AND id=?", [$auth->getUserID(), $_POST['subscription']]);
-    echo "Follow";
+    echo $localization->translate("follow");
 } else {
     $database->query("INSERT INTO user_follows (id, user) VALUES (?,?)", [$_POST['subscription'], $auth->getUserID()]);
-    echo "Unfollow";
+    echo $localization->translate("unfollow");
 }
