22539175008556c48e76f02cceb5225f082dbfd8
Chaziz <chazpelo2@gmail.com>
2026-01-20 08:25:16 -0500

    add following user uploads on homepage, fix missing copyright banner on guidelines.php

--- private/pages/guidelines.php
+++ private/pages/guidelines.php
@@ -1,4 +1,24 @@
 <?php
+
+/*
+  OpenSB: The Open SquareBracket Software
+
+  Copyright (C) 2023-2026 Chaziz
+
+  OpenSB is free software: you can redistribute it and/or modify it under the 
+  terms of the GNU Affero General Public License as published by the Free 
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version. 
+
+  OpenSB is distributed in the hope that it will be useful, but WITHOUT ANY 
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more 
+  details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // ported from principia-web by chaziz -4/20/2023
 namespace OpenSB\Pages;
 
