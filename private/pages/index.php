62986ee40bd4fa1d6803d8f6e1e81b8765a6f652
Chaziz <chazpelo2@gmail.com>
2026-01-20 08:25:16 -0500

    add following user uploads on homepage, fix missing copyright banner on guidelines.php
    ok i said no new features but whatever

--- private/pages/index.php
+++ private/pages/index.php
@@ -23,13 +23,15 @@
 
 namespace OpenSB\Pages;
 
-global $twig, $database, $sb;
+global $twig, $database, $sb, $auth;
 
 use OpenSB\UploadFlags;
 use OpenSB\UploadQuery;
 use OpenSB\Utilities;
 use OpenSB\UserData;
 use OpenSB\UserFlags;
+
+// this page is fucked up and should be cleaned up in 2.1
 
 $upload_query = new UploadQuery($sb);
 
@@ -78,6 +80,34 @@ $uploads_featured = $upload_query->query(
     sprintf("v.flags & %d = %d", UploadFlags::FLAG_FEATURED->value, UploadFlags::FLAG_FEATURED->value)
 );
 
+if ($options["skin"] == "trinium" & $auth->isUserLoggedIn()) { // TODO: bootstrap had this too back then
+    // copied from SquareBracketTwigExtension
+    $rows = $database->fetchArray(
+        $database->query(
+            "SELECT s.* FROM user_follows s
+            JOIN users u ON s.user = u.id
+            WHERE s.user = ?
+            AND s.id NOT IN (SELECT user FROM user_bans)",
+            [$auth->getUserID()]
+        )
+    );
+
+    if ($rows) {
+        $users = array_column($rows, 'id');
+        $query = implode(', ', $users);
+
+        $uploads_following = $upload_query->query(
+            "v.timestamp DESC",
+            $uploads_featured_query_limit,
+            sprintf("v.author in (%s)", $query)
+        );
+    } else {
+        $uploads_following = [];
+    }
+} else {
+    $uploads_following = [];
+}
+
 $news_recent = $database->fetchArray($database->query("SELECT j.* FROM journals j WHERE j.is_news = 1 ORDER BY j.timestamp DESC LIMIT $news_recent_query_limit"));
 
 if ($options["skin"] == "trinium") {
@@ -115,19 +145,10 @@ $data = [
     "uploads" => Utilities::makeUploadArray($database, $uploads_random),
     "uploads_new" => Utilities::makeUploadArray($database, $uploads_recent),
     "uploads_featured" => Utilities::makeUploadArray($database, $uploads_featured),
+    "uploads_following" => Utilities::makeUploadArray($database, $uploads_following),
     "news_recent" => Utilities::makeJournalArray($database, $news_recent),
     "users_recent" => $users_recent,
 ];
-
-
-/*
-if ($options["skin"] == "trinium" && !$sb->getAuthenticationClass()->isUserLoggedIn()) {
-    if (empty($_SESSION['csrf_token'])) {
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(16));
-    }
-    $data['csrf_token'] = $_SESSION['csrf_token'];
-}
-*/
 
 echo $twig->render('index.twig', [
     'data' => $data,
