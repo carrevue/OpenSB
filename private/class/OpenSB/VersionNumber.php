2f9744e8b724dadc1825fc4a33c9be973506c71f
Chaziz <chazpelo2@gmail.com>
2026-02-14 18:32:31 -0500

    bump to 2.1.0 alpha 3

--- private/class/OpenSB/VersionNumber.php
+++ private/class/OpenSB/VersionNumber.php
@@ -41,7 +41,7 @@ class VersionNumber
     /**
      * @var string The version number, which tries to follow Semantic versioning.
      */
-    private string $versionNumber = "2.1.0-alpha.2";
+    private string $versionNumber = "2.1.0-alpha.3";
 
     /**
      * @var string The current Git branch.
