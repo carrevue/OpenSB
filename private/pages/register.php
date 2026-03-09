0dbe6c8f1a8a4716d8745c79f30fe97cb4f982b6
Chaziz <chazpelo2@gmail.com>
2026-03-09 14:19:23 -0400

    readd stopforumspam api support
    haha, fuck.

--- private/pages/register.php
+++ private/pages/register.php
@@ -44,6 +44,12 @@ if ($sb->isIpLookupEnabled() && $sb->isChazizInstance()) {
     }
 }
 
+$ipcheck = file_get_contents("https://api.stopforumspam.org/api?ip=" . Utilities::getIpAddress());
+
+if (str_contains($ipcheck, "<appears>yes</appears>") && !$isDebug) {
+    Utilities::notifyBanner("notify_register_ip_suspicious", "/");
+}
+
 $captcha = $sb->getCaptchaSettings();
 
 // tip: if youre hosting opensb on a linux distro with selinux included (eg: fedora) and you get some
