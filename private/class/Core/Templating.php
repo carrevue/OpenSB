068125f623ece05f44e44ae0e2a60136b3cb785a
Chaziz <chazpelo2@gmail.com>
2026-05-07 06:42:29 -0400

    fix trinium upload grids, initial reindex user info script

--- private/class/Core/Templating.php
+++ private/class/Core/Templating.php
@@ -129,11 +129,6 @@ class Templating
         $this->skin = $this->options["skin"] ?? $default_skin;
         $this->theme = $this->options["theme"] ?? $default_theme;
 
-        if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Windows NT 5.1')) {
-            $this->skin = "retro";
-            $this->theme = "default";
-        }
-
         if ($this->skin === null || trim($this->skin) === '') {
             $this->resetToDefault();
         }
---
+++ private/scripts/reindex_user_info.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+  OpenSB: The Open SquareBracket Software
+
+  Copyright (C) 2026 Chaziz
+
+  OpenSB is free software: you can redistribute it and/or modify it under the 
+  terms of the GNU Affero General Public License as published by the Free 
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version. 
+
+  OpenSB is distributed in the hope that it will be useful, but WITHOUT ANY 
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more 
+  details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace OpenSB;
+
+define("SB_ROOT_PATH", dirname(__DIR__, 2));
+define("SB_PUBLIC_PATH", SB_ROOT_PATH . '/public'); // we need this for SquareBracketTwigExtension
+define("SB_PRIVATE_PATH", SB_ROOT_PATH . '/private');
+define("SB_VENDOR_PATH", SB_ROOT_PATH . '/vendor');
+define("SB_GIT_PATH", SB_ROOT_PATH . '/.git'); // ONLY FOR makeVersionString() IN SquareBracket CLASS.
+
+require_once SB_PRIVATE_PATH . '/common.php';
+
+global $database;
+
+$users = $database->fetchArray($database->query("SELECT * FROM users"));
+
+foreach ($users as $user) {
+    $numbers = $database->fetch("SELECT * FROM (
+        SELECT u.id,
+            (SELECT COUNT(*) FROM uploads WHERE author = u.id AND upload_id NOT IN (SELECT upload from upload_takedowns)) AS u_num,
+            (SELECT COUNT(user) FROM user_follows WHERE id = u.id AND user NOT IN (SELECT user from user_bans)) AS f_num
+        FROM users u
+    ) AS u where u.id = ?", [$user["id"]]);
+
+    $database->query(
+        "UPDATE users SET u_index = ?, f_index = ? WHERE id = ?",
+        [$numbers["u_num"], $numbers["f_num"], $user["id"]]
+    );
+}
+
+if ($sb->isDiscordWebhookEnabled()) {
+    $sb->getDiscordWebhookClass()->scriptSuccessHook(__FILE__);
+}
