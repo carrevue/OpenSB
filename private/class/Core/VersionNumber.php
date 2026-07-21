d837e031571a8a9d0f2e9c61419ae9003ad38716
Chaziz <chazpelo2@gmail.com>
2026-07-21 05:03:31 -0400

    let's consider 2.1 complete
    something i've noticed is that each "release" of opensb keeps having a bloated scope. opensb 2.0 started development in october 2024 and was considered complete in september 2025. that's around 11 months. at this pace, if i finish 2.1 with everything i have in mind, it would end up being opensb 3.0, which is stupid.

--- private/class/Core/VersionNumber.php
+++ private/class/Core/VersionNumber.php
@@ -40,7 +40,7 @@ class VersionNumber
     /**
      * @var string The version number, which tries to follow Semantic versioning.
      */
-    private string $versionNumber = "2.1.0-beta.2.1";
+    private string $versionNumber = "2.1.0";
 
     /**
      * @var string The current Git branch.
